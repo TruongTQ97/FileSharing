134754688b695a2651e41ac4d9a2763b14c51365
TruongTQ97 <69665917+TruongTQ97@users.noreply.github.com>
2020-09-26 15:43:55 +0700

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,16 @@
+## Steps to Setup
+
+
+```
+
+** Configure MySQL database**
+
+Create a MySQL database named `file_demo`, and change the username and password in `src/main/resources/application.properties` as per your MySQL
+installation -
+
+```properties
+spring.datasource.username= <YOUR MYSQL USERNAME>
+spring.datasource.password= <YOUR MYSQL PASSWORD>
+```
+
+The application can be accessed at `http://localhost:8081`.
---
+++ insertDataToUserTable.sql
@@ -0,0 +1,8 @@
+insert into filesharing.user values(1,"Nguyen Van A","khongcopassdau");
+insert into filesharing.user values(2,"Nguyen Van B","khongcopassdau");
+insert into filesharing.user values(3,"Nguyen Van C","khongcopassdau");
+insert into filesharing.user values(4,"Nguyen Van D","khongcopassdau");
+insert into filesharing.user values(5,"Nguyen Van E","khongcopassdau");
+insert into filesharing.user values(6,"Nguyen Van F","khongcopassdau");
+insert into filesharing.user values(7,"Nguyen Van G","khongcopassdau");
+insert into filesharing.user values(8,"Nguyen Van H","khongcopassdau");
